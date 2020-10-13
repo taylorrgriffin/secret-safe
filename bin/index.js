203878e124b8198548e296c3d245076116a7838a
Taylor Griffin <taylor.griffin.365@gmail.com>
2020-10-13 00:16:11 -0700

    Function to check status of config, pull config from package.json

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { init } = require('../lib/init');
 const { unlink } = require('../lib/unlink');
+const { status } = require('../lib/status');
 const { getConfig } = require('../lib/utils');
 const { preCommit } = require('../lib/pre-commit');
 const { postCommit } = require('../lib/post-commit');
@@ -19,6 +20,9 @@ else if (args[0] == 'init') {
 }
 else if (args[0] == "unlink") {
   unlink(config);
+}
+else if (args[0] == "status") {
+  status(config);
 }
 else if (args[0] == "pre-commit") {
   preCommit(config);
