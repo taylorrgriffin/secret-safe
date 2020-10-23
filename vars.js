ce0e40fd60d82388be07185cccbe9ee2dd294ea6
Taylor Griffin <taylor.griffin.365@gmail.com>
2020-10-23 13:42:45 -0700

    Finished main functionality

--- vars.js
+++ vars.js
@@ -1,10 +1,16 @@
+const path = require("path");
+
 const preCommitHooksFile = ".git/hooks/pre-commit";
 const postCommitHooksFile = ".git/hooks/post-commit";
 
-const secretSafePreCommit = "secret-safe pre-commit";
-const secretSafePostCommit = "secret-safe post-commit";
+const secretSafePreCommit = "./node_modules/@taylorrgriffin/secret-safe/bin/index.js pre-commit";
+const secretSafePostCommit = "./node_modules/@taylorrgriffin/secret-safe/bin/index.js post-commit";
+// const secretSafePreCommitGlobal = "secret-safe pre-commit";
+// const secretSafePostCommitGlobal = "secret-safe post-commit";
 
 const SECRET_SAFE_TMP_DIR = '.git/secret-safe-tmp';
+const LEDGER = path.join(SECRET_SAFE_TMP_DIR, "ledger.json");
+
 const GIT_STAGED_FILES = "git diff --cached --name-only --diff-filter=ACMR";
 
 module.exports = {
@@ -14,4 +20,5 @@ module.exports = {
   secretSafePostCommit,
   SECRET_SAFE_TMP_DIR,
   GIT_STAGED_FILES,
+  LEDGER,
 };
